2891ce2408927180aa3fceb19a9478e174195d65
Wilson carvajal <wgcarvajal@unicauca.edu.co>
2018-02-12 01:19:10 -0500

    feat: agregando modulo(lamemp3) necesario para la creacion de audios en formato mp3.

--- app/src/main/java/proyecto/com/domos/ui/fragments/shop/ShopFragment.java
+++ app/src/main/java/proyecto/com/domos/ui/fragments/shop/ShopFragment.java
@@ -1,18 +1,37 @@
 package proyecto.com.domos.ui.fragments.shop;
 
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import proyecto.com.domos.R;
+import proyecto.com.domos.util.AudioRecordHandler;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ShopFragment extends Fragment {
+public class ShopFragment extends Fragment implements View.OnClickListener {
+
+
+    private static final int IDLE = 0;
+    private static final int RECORDING = 1;
+    private static final String TAG = ShopFragment.class.getSimpleName();
+
+
+    private ImageView mBtnRecord;
+    private EditText mPath;
+
+    private int status = IDLE;
+
+    private AudioRecordHandler mAudioRecordInstance;
 
 
     public ShopFragment() {
@@ -25,7 +44,66 @@ public class ShopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_shop, container, false);
+        mBtnRecord = view.findViewById(R.id.btn_record);
+        mPath = view.findViewById(R.id.et_path);
+
+        mBtnRecord.setOnClickListener(this);
         return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_record:
+                onRecordClick();
+                break;
+        }
+    }
+
+
+    private void onRecordClick() {
+        if (status == IDLE) {
+            doActionRecord();
+            status = RECORDING;
+            mBtnRecord.setImageResource(R.mipmap.ic_record_pause);
+        } else if (status == RECORDING) {
+            doActionStopRecord();
+            status = IDLE;
+            mBtnRecord.setImageResource(R.mipmap.ic_record_start);
+        }
+    }
+
+    private void doActionRecord() {
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.RECORD_AUDIO);
+        Log.i(TAG, "i =" + permissionCheck);
+        //开始录音
+        mAudioRecordInstance = new AudioRecordHandler("/sdcard/hello.mp3",
+                new AudioRecordHandler.ProgressListener() {
+                    @Override
+                    public boolean reportProgress(double duration) {
+                        Log.i(TAG, "duration = " + duration);
+                        return true;
+                    }
+                });
+        Thread th = new Thread(mAudioRecordInstance);
+        th.start();
+        mAudioRecordInstance.setRecording(true);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mAudioRecordInstance != null) {
+            mAudioRecordInstance.setRecording(false);
+        }
+    }
+
+    private void doActionStopRecord() {
+        if (mAudioRecordInstance != null) {
+            mAudioRecordInstance.setRecording(false);
+        }
+    }
 }
---
+++ app/src/main/java/proyecto/com/domos/util/AudioRecordHandler.java
@@ -0,0 +1,201 @@
+package proyecto.com.domos.util;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import proyecto.com.lamemp3.Encoder;
+
+/**
+ * @Description:　录音
+ * @author: Created by martin on 16-3-22.
+ */
+public class AudioRecordHandler implements Runnable {
+
+    private static final String TAG = AudioRecordHandler.class.getSimpleName();
+
+    /****
+     * 采样率
+     */
+    private static final int SAMPLERATEINHZ = 44100;
+
+    /****
+     * 设置音频的录制的声道CHANNEL_IN_STEREO为双声道，CHANNEL_CONFIGURATION_MONO为单声道
+     */
+    private static final int CHANNELCONFIG = AudioFormat.CHANNEL_IN_MONO;
+
+    /***
+     * 音频数据格式:PCM 16位每个样本
+     */
+    private static final int AUDIOFORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    /***
+     * 音频源：指的是从哪里采集音频。这里我们当然是从麦克风采集音频，所以此参数的值为MIC
+     */
+    private static final int AUDIOSOURCE = MediaRecorder.AudioSource.MIC;
+    private Encoder encoder;
+
+    private AudioRecord audioRecord;
+
+    private String filePath;
+
+    private FileOutputStream outputStream;
+
+    private Object mutex = new Object();
+
+    //是否暂停
+    private boolean paused;
+    //是否正在录音
+    private boolean isRecording;
+
+    private int recordTime;
+
+    public AudioRecordHandler(String filePath, ProgressListener progressListener) {
+        this.filePath = filePath;
+        this.mProgressListener = progressListener;
+        this.encoder = new Encoder.Builder(SAMPLERATEINHZ, CHANNELCONFIG, SAMPLERATEINHZ, 16).create();
+    }
+
+
+    @Override
+    public void run() {
+        int bufferSize = AudioRecord.getMinBufferSize(SAMPLERATEINHZ, CHANNELCONFIG, AUDIOFORMAT);
+
+        try {
+            outputStream = new FileOutputStream(new File(filePath));
+            audioRecord = new AudioRecord(AUDIOSOURCE, SAMPLERATEINHZ, CHANNELCONFIG, AUDIOFORMAT, bufferSize);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            //todo:通知录音失败
+            return;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            //todo:通知录音失败
+            releaseAndCloseStream();
+            return;
+        }
+
+        short[] buffer = new short[SAMPLERATEINHZ * 2 * 5];
+        byte[] mp3buffer = new byte[(int) (7200 + buffer.length * 2 * 1.25)];
+
+        audioRecord.startRecording();
+
+        int bufferRead = 0;
+        int duration = 0;
+
+        while (isRecording) {
+
+            while (isPaused()) {
+                try {
+                    if (!isRecording)
+                        break;
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            bufferRead = audioRecord.read(buffer, 0, bufferSize);
+            recordTime = duration / SAMPLERATEINHZ;
+
+            Log.i(TAG, "record time =" + recordTime);
+
+            if (isRecording && !mProgressListener.reportProgress(recordTime)) {
+                break;
+            }
+
+            if (bufferRead > 0) {
+                duration += bufferRead;
+
+                Log.d(TAG, "encoding bytes to mp3 buffer......duration=" + duration);
+                int bytesEncoded = encoder.encode(buffer, buffer, bufferRead, mp3buffer);
+
+                if (bytesEncoded > 0) {
+                    try {
+                        outputStream.write(mp3buffer, 0, bytesEncoded);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                releaseAndCloseStream();
+                //todo:通知录音失败
+                return;
+            }
+        }
+
+        int outputMp3buf = encoder.flush(mp3buffer);
+
+        if (outputMp3buf > 0) {
+            try {
+                outputStream.write(mp3buffer, 0, outputMp3buf);
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        release();
+    }
+
+    private void releaseAndCloseStream() {
+        try {
+            if (outputStream != null)
+                outputStream.close();
+            release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void release() {
+        audioRecord.stop();
+        audioRecord.release();
+
+        encoder.close();
+
+        isRecording = false;
+        audioRecord = null;
+    }
+
+    public void setPaused(boolean paused) {
+        synchronized (mutex) {
+            this.paused = paused;
+        }
+    }
+
+    public boolean isPaused() {
+        synchronized (mutex) {
+            return paused;
+        }
+    }
+
+    public void setRecording(boolean isRec) {
+        synchronized (mutex) {
+            this.isRecording = isRec;
+        }
+    }
+
+    public boolean isRecording() {
+        synchronized (mutex) {
+            return isRecording;
+        }
+    }
+
+    private ProgressListener mProgressListener;
+
+    public int getRecordTime() {
+        return recordTime;
+    }
+
+    // Progress listener interface.
+    public interface ProgressListener {
+
+        boolean reportProgress(double fractionComplete);
+    }
+}
